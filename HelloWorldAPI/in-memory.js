f58352439339fe876fbbbfd4739c2c7302cb1986
Ed Fernando Villanueva <villanuevaedfernando@gmail.com>
2025-09-13 14:01:29 +0800

    Challenge 3 done.

--- HelloWorldAPI/in-memory.js
+++ HelloWorldAPI/in-memory.js
@@ -1,26 +1,24 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import morgan from 'morgan';
 import postRoutes from './src/routes/post.routes.js';
 import productRoutes from './src/routes/product.routes.js';
 import commentRoutes from './src/routes/comment.routes.js';
-
-dotenv.config();
+import config from './src/config/index.js';
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('tiny'));
+if (config.nodeEnv === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
 app.use('/posts', postRoutes);
 app.use('/products', productRoutes);
-app.use('/comments', commentRoutes); 
+app.use('/comments', commentRoutes);
 
-app.listen(port, ()=> {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(config.port, () => {
+  console.log(
+    `Server is running at http://localhost:${config.port} in ${config.nodeEnv} mode`
+  );
 });
