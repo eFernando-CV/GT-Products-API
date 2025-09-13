c4dd357369063828a44e33034271807f05eea552
Ed Fernando Villanueva <villanuevaedfernando@gmail.com>
2025-09-13 14:35:45 +0800

    Done with 7. Database connection.

--- HelloWorldAPI/in-memory.js
+++ HelloWorldAPI/in-memory.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
+
 import postRoutes from './src/routes/post.routes.js';
 import productRoutes from './src/routes/product.routes.js';
 import commentRoutes from './src/routes/comment.routes.js';
+
+import { testConnection } from './src/config/db.js';
 import config from './src/config/index.js';
 
 const app = express();
@@ -18,7 +21,6 @@ app.use('/products', productRoutes);
 app.use('/comments', commentRoutes);
 
 app.listen(config.port, () => {
-  console.log(
-    `Server is running at http://localhost:${config.port} in ${config.nodeEnv} mode`
-  );
+  console.log(`Server is running on http://localhost:${config.port}`);
+  testConnection(); 
 });
--- HelloWorldAPI/src/routes/post.routes.js
+++ HelloWorldAPI/src/routes/post.routes.js
@@ -7,7 +7,7 @@ router.get('/', postController.getAllPosts);
 router.post('/', postController.createPost);
 router.get('/:id', postController.getPostById);
 router.put('/:id', postController.updatePost);
+router.patch('/:id', postController.partiallyUpdatePost);
 router.delete('/:id', postController.deletePost);
-router.patch('/:id', postController.patchPost);
 
 export default router;
