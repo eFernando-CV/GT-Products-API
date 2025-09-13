4db7125c856f8b69d8252614312b78ecf3324ac0
Ed Fernando Villanueva <villanuevaedfernando@gmail.com>
2025-09-13 13:56:34 +0800

    Challenge 1 done.

--- HelloWorldAPI/in-memory.js
+++ HelloWorldAPI/in-memory.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
 import postRoutes from './src/routes/post.routes.js';
 import productRoutes from './src/routes/product.routes.js';
 import commentRoutes from './src/routes/comment.routes.js';
 
+dotenv.config();
+
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
 app.use('/posts', postRoutes);
 app.use('/products', productRoutes);
 app.use('/comments', commentRoutes); 
