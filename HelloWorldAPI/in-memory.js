1d5dc4e910d9395069c343fbb9122b2270fffe06
Ed Fernando Villanueva <villanuevaedfernando@gmail.com>
2025-09-13 13:58:36 +0800

    Challenge 2 done.

--- HelloWorldAPI/in-memory.js
+++ HelloWorldAPI/in-memory.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('tiny'));
 }
 
 app.use(express.json());
