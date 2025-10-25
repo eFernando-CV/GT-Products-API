3d70fe1968dd209f9fb7d88f02265d0664dd0816
Ed Fernando Villanueva <villanuevaedfernando@gmail.com>
2025-10-25 20:50:04 +0800

    14. Multi-part-upload done.

--- HelloWorldAPI/index.js
+++ HelloWorldAPI/index.js
@@ -9,6 +9,7 @@ import productRoutes from './src/routes/product.routes.js';
 import commentRoutes from './src/routes/comment.routes.js';
 import userRoutes from './src/routes/user.routes.js';
 import authRoutes from './src/routes/auth.routes.js';
+import photoRoutes from './src/routes/photo.routes.js';
 
 import { testConnection } from './src/config/db.js';
 import { errorHandler } from './src/middlewares/errorHandler.middleware.js';
@@ -28,6 +29,8 @@ app.use('/api/posts', postRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/users', userRoutes);
+app.use('/uploads', express.static('uploads'));
+app.use('/api/photos', photoRoutes);
 
 app.use(errorHandler);
 
