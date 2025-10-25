5bff7673d6846f34197c3d31836af0e8e6e9a742
Ed Fernando Villanueva <villanuevaedfernando@gmail.com>
2025-10-25 20:15:12 +0800

    13.Authorization done. Re-push

--- HelloWorldAPI/src/middlewares/errorHandler.middleware.js
+++ HelloWorldAPI/src/middlewares/errorHandler.middleware.js
@@ -1,21 +1,25 @@
 import { ApiError } from '../utils/ApiError.js';
 
 export const errorHandler = (err, req, res, next) => {
-  console.error(err);
+    let statusCode = err.statusCode || 500;
+    let message = err.message || "Internal Server Error";
 
-  let statusCode = 500;
-  let message = "Internal Server Error";
+    if (!(err instanceof ApiError)) {
+        console.error(err);
 
-  if (err instanceof ApiError) {
-    statusCode = err.statusCode;
-    message = err.message;
-  } else if (err.code === 'ER_NO_REFERENCED_ROW_2') {
-    statusCode = 400;
-    message = 'Invalid postId or authorId. The specified post or user does not exist.';
-  }
+        if (process.env.NODE_ENV === 'production') {
+            message = "Something went wrong!";
+        }
+    }
 
-  return res.status(statusCode).json({
-    success: false,
-    message: message,
-  });
+    const response = {
+        success: false,
+        message: message,
+    };
+
+    if (process.env.NODE_ENV === 'development') {
+        response.stack = err.stack;
+    }
+
+    res.status(statusCode).json(response);
 };
